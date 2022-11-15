40bddbc4fa2a5c226449b84ef277bb22d330e3a5
NaokiSakai <zeal20410222@fsgcl.jp>
2022-11-15 13:53:19 +0900

    プロジェクト作成
    ASP.NETMVCフレームワークのプロジェクトを作成

---
+++ TeamHApp/TeamHApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TeamHApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
